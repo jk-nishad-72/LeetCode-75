15f00dcc1cd35733e383302b6423c76c5ff719ef
jk-nishad-72 <jkn18869@gmail.com>
2026-04-27 11:33:05 +0530

    #day 3 solved

--- Array-String/Day3.js
+++ Array-String/Day3.js
@@ -1,3 +1,26 @@
 
+/**—>Q.1431. Kids With the Greatest Number of Candies ✅(fnd Max , cmpr each ith + extrC > mx)  
+ * 
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    
+    let ans = new Array(candies.length).fill(false) 
 
-console.log("Day 3: Array and String Manipulation");
+    let max = 0;
+    for(let i = 0;i<candies.length;i++){
+          max = Math.max(candies[i], max)
+    }
+
+    for(let j = 0;j<candies.length;j++){
+         
+          if((candies[j] +extraCandies) >= max ){
+              ans[j] = true
+          }
+    }
+    return ans
+};
+
+console.log(kidsWithCandies(candies = [2,3,5,1,3], extraCandies = 3));
