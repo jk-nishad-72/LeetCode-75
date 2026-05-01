79f01b2c692091c59297f22d7fda9f1d085f85d4
jk-nishad-72 <jkn18869@gmail.com>
2026-05-01 23:38:21 +0530

    solved

--- Array-String/Day7.js
+++ Array-String/Day7.js
@@ -2,3 +2,26 @@
 
 console.log("Day 7: Array and String Manipulation");
 
+/**151. Reverse Words in a String
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    
+    let words = s.split(" ");
+    //  console.log(words);
+
+     let ans = ''
+     for(let i = words.length-1;i>=0;i--){
+            if(words[i] !== ''){
+                ans += words[i] + ' ';
+            } 
+     }
+    ans = ans.trim();
+    return ans;
+
+   
+};
+
+console.log(reverseWords('hello word'));
+
