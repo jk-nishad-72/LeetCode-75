9f58b1d53237019bd168078e08caf254ed3c9db0
jk-nishad-72 <jkn18869@gmail.com>
2026-05-04 22:18:05 +0530

    done

--- Two-pointer/Day10.js
+++ Two-pointer/Day10.js
@@ -1,2 +1,45 @@
+/**283. Move Zeroes
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
 
-console.log("Two-pointer/Day10.js —>Q. 283. Move Zeroes ✅");
+// Approach 1: Brute Force using extra space
+// var moveZeroes = function(nums){
+ 
+//     let ans = new Array(nums.length).fill(0)
+
+//  let j = 0;
+
+//  for(let i   = 0;i<nums.length ; i++){
+
+//          if(nums[i] != 0){
+//             ans[j++] = nums[i]
+//          }
+//  }
+
+//  return ans
+// };
+
+// Approach 1: Brute Force using extra space
+var moveZeroes = function(nums){
+ 
+    let i = 0
+
+ let j = 0;
+
+while(j < nums.length){
+
+         if(nums[j] != 0){
+            let temp = nums[i]
+            nums[i++] = nums[j]
+            nums[j] = temp
+         }
+         else j++
+ }
+
+ return nums
+};
+
+
+console.log(moveZeroes(nums = [0,1,0,3,12]));
+console.log(moveZeroes(nums = [0]));
