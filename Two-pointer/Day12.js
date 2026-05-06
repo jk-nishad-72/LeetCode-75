0d4031453f80bbe03a62c3e282b805b122edd426
jk-nishad-72 <jkn18869@gmail.com>
2026-05-06 22:26:48 +0530

    done

--- Two-pointer/Day12.js
+++ Two-pointer/Day12.js
@@ -1,4 +1,19 @@
+/**11. Container With Most Water
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+ let ans = 0;
+let i = 0;
+let j = height.length-1;
+while(i<j){
+ans = Math.max(ans,(Math.min(height[i] , height[j])* (j-i)))
 
+ if(height[i] < height[j]) i++
+ else j--
+}
 
-console.log('Dsa day 12 of leetcode 75 ')
+ return ans
+};
 
+console.log(maxArea([1,8,6,2,5,4,8,3,7])) // 49
