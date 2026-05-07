ec30cffd2b08cc3dee6e8343e488fcb301379589
jk-nishad-72 <jkn18869@gmail.com>
2026-05-07 23:19:10 +0530

    O(n^2) + O(n)  solution

--- Two-pointer/Day13.js
+++ Two-pointer/Day13.js
@@ -1,3 +1,39 @@
 
 
-console.log('Day 13 of leetcode 75 challenge');
+/**1679. Max Number of K-Sum Pairs
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxOperations = function(nums, k) {
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === k) {
+                count++;
+                nums.splice(j, 1);
+                break;
+            }
+        }
+    }
+
+    return count;
+
+    //        let map = new Map();
+    //   let ans = 0
+    // for(let i =0;i<nums.length;i++){
+    //     if(map.has(k-nums[i])){
+         
+    //      map.delete(k-nums[i])
+    //        ans++ 
+          
+    //     }else{
+    //         map.set(nums[i],i)
+    //     }
+    // }
+    // return ans
+};
+
+console.log(maxOperations([1,2,3,4], 5)) // 2
+console.log(maxOperations([3,1,3,4,3], 6)) // 1
