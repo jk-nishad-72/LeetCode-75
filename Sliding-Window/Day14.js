a62574d9e09d6a1677784360718d8c5fc9d13cce
jk-nishad-72 <jkn18869@gmail.com>
2026-05-08 23:44:20 +0530

    brute force done

--- Sliding-Window/Day14.js
+++ Sliding-Window/Day14.js
@@ -1,5 +1,24 @@
+/**643. Maximum Average Subarray I
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    
+
+    let max = -Infinity;
+
+    for (let i = 0; i <= nums.length - k; i++) {
+        let sum = 0;
+        for (let j = i; j < i + k; j++) {
+            sum += nums[j];
+        }
+        max = Math.max(max, sum / k);
+    }
+    return max;
 
 
-console.log("Day 14: Sliding Window");
+};
 
-// Sliding Window is a technique used to solve problems that involve finding a contiguous subarray or substring that satisfies certain conditions. It involves maintaining a window of a certain size and sliding it across the input data to find the desired result.
+console.log(findMaxAverage([1,12,-5,-6,50,3], 4)) // 12.75
+console.log(findMaxAverage([5], 1)) // 5
