62b92c5e6c6e117b5c85c2b140f995e2cfb3b475
jk-nishad-72 <jkn18869@gmail.com>
2026-05-08 23:57:42 +0530

    solved sliding window '

--- Sliding-Window/Day14.js
+++ Sliding-Window/Day14.js
@@ -20,5 +20,28 @@ var findMaxAverage = function(nums, k) {
 
 };
 
+var findMaxAverageOptimise = function(nums, k) {
+
+    let i = 0;
+    let j = 0;
+    let sum = 0;
+    let max = -Infinity;
+
+    while(j < nums.length){
+        sum += nums[j];
+
+        if(j - i + 1 === k){
+            max = Math.max(max , sum/k);
+            sum -= nums[i]
+            i++
+        }
+        j++
+    }
+    return max;
+}
+
 console.log(findMaxAverage([1,12,-5,-6,50,3], 4)) // 12.75
 console.log(findMaxAverage([5], 1)) // 5
+
+console.log(findMaxAverageOptimise([1,12,-5,-6,50,3], 4)) // 12.75
+console.log(findMaxAverageOptimise([5], 1)) // 5
