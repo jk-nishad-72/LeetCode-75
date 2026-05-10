900801be16dd8c009ef4a100075b223e8fac35aa
jk-nishad-72 <jkn18869@gmail.com>
2026-05-10 23:00:27 +0530

    day 16 of LC 75

---
+++ Sliding-Window/Day16.js
@@ -0,0 +1,47 @@
+
+//count occurrences of anagrams of pattern in text
+
+class Solution {
+    countAnagrams(txt, pat) {
+        // Write your code here 
+      let i = 0, j = 0, count = 0;
+      let textarr = new Array(26).fill(0);
+      let patarr = new Array(26).fill(0);
+
+        for(let k = 0; k < pat.length; k++){
+            patarr[pat.charCodeAt(k) - 97]++;
+        }
+
+      let k = pat.length;
+      while(j < txt.length){
+         textarr[txt.charCodeAt(j) - 97]++;
+     
+         if(j - i + 1 == k){
+             if(this.anagram(textarr, patarr )) count++;
+             textarr[txt.charCodeAt(i) - 97]--;
+             i++;
+             
+         }
+         j++; 
+      }
+        return count;
+        
+    }
+
+    anagram(textarr , patarr){
+         
+         for(let i = 0; i < 26; i++){
+             if(textarr[i] != patarr[i]) return false
+         }
+         return true
+    }
+}
+
+
+
+const txt = "aabbaabb";
+const pat = "ab";
+
+const obj = new Solution();
+const ans = obj.countAnagrams(txt, pat);
+console.log(ans);
