1ac9aa5f22441e71423f878057e52f8c0ea7f066
jk-nishad-72 <jkn18869@gmail.com>
2026-05-13 09:55:31 +0530

    done

--- Prefix-Sum/Day19.js
+++ Prefix-Sum/Day19.js
@@ -1,3 +1,21 @@
+/**1732. Find the Highest Altitude
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function(gain) {
+    let n  = gain.length;
+    let altitude = new Array(n+1).fill(0);
+    let highestAlt = altitude[0];
 
+    for(let i =1 ;i< n+1 ; i++){
+         altitude[i] = gain[i-1] + altitude[i-1]
+         highestAlt = Math.max(highestAlt , altitude[i])
+    }
 
-console.log("Day 19 leetcode 75 challenge");
+    return highestAlt
+};
+
+console.log(largestAltitude([-5,1,5,0,-7])) // 1
+
+console.log(largestAltitude([-4,-3,-2,-1,4,3,2])) // 0
+
