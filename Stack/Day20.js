e89b1c43b4d8b5e27d2d50124012b2da8a47e693
jk-nishad-72 <jkn18869@gmail.com>
2026-05-14 12:13:15 +0530

    done

--- Stack/Day20.js
+++ Stack/Day20.js
@@ -1,2 +1,22 @@
+/**2390. Removing Stars From a String
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function(s) {
+    let stack = new Array();
+    let ans = ""
 
-console.log('Day 20 of LC 75 challenge ')
+    for(let i = 0;i<s.length;i++){
+
+         if(s[i] === '*') stack.pop()
+         else stack.push(s[i])
+    }
+    for(let i = 0;i<stack.length;i++){
+
+          ans += stack[i]
+    }
+    return ans
+};
+
+console.log(removeStars('leet**cod*e'))
+console.log(removeStars('abced*****'))
