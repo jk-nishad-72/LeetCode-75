748bf6ef09fb78ac78cbfad954e99e09c81e504d
jk-nishad-72 <jkn18869@gmail.com>
2026-05-15 12:31:33 +0530

    solved

--- Stack/Day21.js
+++ Stack/Day21.js
@@ -1,3 +1,42 @@
+/**
+735. Asteroid Collision
+ * @param {number[]} asteroids
+ * @return {number[]}
+ */
+var asteroidCollision = function(asteroids) {
+    let stack = [];
 
+    for (let i = 0; i < asteroids.length; i++) {
+        let curr = asteroids[i];
+        let destroyed = false;
 
-console.log("Day 21 of leetcode 75 ")
+        while (
+            stack.length &&
+            stack[stack.length - 1] > 0 &&
+            curr < 0
+        ) {
+            let top = stack[stack.length - 1];
+
+            if (top === -curr) {
+                stack.pop();
+                destroyed = true;
+                break;
+            } else if (top < -curr) {
+                stack.pop(); // keep checking
+            } else {
+                destroyed = true;
+                break;
+            }
+        }
+
+        if (!destroyed) {
+            stack.push(curr);
+        }
+    }
+
+    return stack;
+};
+
+console.log(asteroidCollision([5,10,-5]))
+console.log(asteroidCollision([8,-8]))
+console.log(asteroidCollision([3 , 5 , -6 , 2 ,-1,4]))
