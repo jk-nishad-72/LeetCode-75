4a500b5f9871cf08ed8189fe58e415e1f4f20810
jk-nishad-72 <jkn18869@gmail.com>
2026-05-17 12:17:35 +0530

    done

--- Hashing-Set/Day23.js
+++ Hashing-Set/Day23.js
@@ -1,4 +1,34 @@
+/**2215. Find the Difference of Two Arrays
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function(nums1, nums2) {
+
+    let set1 = new Set(nums1);
+    let set2 = new Set(nums2);
+
+    let ans1 = [];
+    let ans2 = [];
+
+    for(let num of set1){
+
+        if(!set2.has(num)) ans1.push(num)
+        
+    }
+
+    
+    for(let num of set2){
+
+        if(!set1.has(num)) ans2.push(num)
+        
+    }
+
+    
+      return [ans1 , ans2]
 
 
+};
 
-console.log('Day 23 of LC75 challenge');
+console.log(findDifference([1,2,3], nums2 = [2,4,6]))
+console.log(findDifference( [1,2,3,3], nums2 = [1,1,2,]))
