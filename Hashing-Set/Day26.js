b761f20b5fd8b31086dfad1d2b54efbfe998a05d
jk-nishad-72 <jkn18869@gmail.com>
2026-05-20 08:26:01 +0530

    solved

--- Hashing-Set/Day26.js
+++ Hashing-Set/Day26.js
@@ -1,3 +1,28 @@
+/**1207. Unique Number of Occurrences
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function (arr) {
 
+    const map = new Map();
+    let set = new Set();
 
-console.log('Day 26 of LeetCode 75 challenge')
+    for (let n of arr) {
+
+        map.set(n, (map.get(n) || 0) + 1);
+    }
+
+    // Checking of Occurences 
+
+    for (let [key, Occurnec] of map) {
+
+        if (set.has(Occurnec)) return false;
+        else set.add(Occurnec);
+    }
+
+    return true;
+};
+
+console.log(uniqueOccurrences([1, 2, 2, 1, 1, 3]))
+console.log(uniqueOccurrences([1, 2]))
+console.log(uniqueOccurrences([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0]))
