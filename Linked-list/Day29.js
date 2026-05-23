0f71cdd0f3ac6aecd94c01253c5eba95d79af756
jk-nishad-72 <jkn18869@gmail.com>
2026-05-23 23:46:31 +0530

    solve

--- Linked-list/Day29.js
+++ Linked-list/Day29.js
@@ -1,3 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let prev = null, cur = head;
+    while(cur!= null) {
+        let temp = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = temp 
+    }
+    return prev
+};
 
-
-console.log('Day 29 leetcode 75 challenge ')
