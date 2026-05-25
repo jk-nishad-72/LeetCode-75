c98f6fd0de4ea7dfe7174360d86e00f7f9cf21a1
jk-nishad-72 <jkn18869@gmail.com>
2026-05-25 23:51:57 +0530

    solved

--- Binary-Search/Day31.js
+++ Binary-Search/Day31.js
@@ -1,3 +1,22 @@
+/**162. Find Peak Element
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+     
+    let f = 0;
+    let l = nums.length-1;
+    while(f < l) {
 
+         let m = Math.floor((f+l)/2);
 
-console.log("Day 31 of LeetCode 75 - Binary Search");
+         if( nums[m] <= nums[m+1]) f = m+1;
+         else l = m;
+
+    }
+    return f
+};
+
+console.log(findPeakElement([1,2,3,1])) // 2
+console.log(findPeakElement([1,2,1,3,5,6,4])) // 1 or 5
+console.log(findPeakElement([1,2,3,4,5])) // 4
