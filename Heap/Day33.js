eedd5038b13273122b194cd04f591ab76102e51a
jk-nishad-72 <jkn18869@gmail.com>
2026-05-27 23:57:28 +0530

    solved

--- Heap/Day33.js
+++ Heap/Day33.js
@@ -1,2 +1,14 @@
+/**215. Kth Largest Element in an Array
 
-console.log('Day 33 of LC 75 ')
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+
+         nums.sort((a,b)=> b - a  )
+         return nums[k-1]
+
+};
+
+console.log(findKthLargest(nums = [3,2,1,5,6,4], k = 2))
