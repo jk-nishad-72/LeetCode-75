54bf293e78690a5a805762fccd79386a6a60ee7f
jk-nishad-72 <jkn18869@gmail.com>
2026-05-28 23:19:22 +0530

    solved

--- Bit-Manipulation/Day34.js
+++ Bit-Manipulation/Day34.js
@@ -1,3 +1,21 @@
+/**136. Single Number
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+     let map = new Map()
+      for(let i = 0;i<nums.length;i++){
+           if(map.has(nums[i])) {       
+              map.set(nums[i],map.get(nums[i])+1)
+           }else{
+            map.set(nums[i],1)
+           }
+      }
+      for(let i = 0;i<nums.length;i++){
+         
+         if( map.get(nums[i]) == 1) return nums[i]
+      }
+};
 
-console.log(' Day 34 of Leetcode 75 challenge ') 
-
+console.log(singleNumber([2,2,1]))
+console.log(singleNumber([4,1,2,1,2]))
