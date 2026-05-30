41de763ed058bac3ecc7e37096913d4c7ee44639
jk-nishad-72 <jkn18869@gmail.com>
2026-05-30 23:55:30 +0530

    solved

--- BinaryTree-DFS/Day36.js
+++ BinaryTree-DFS/Day36.js
@@ -1,2 +1,21 @@
+/**104. Maximum Depth of Binary Tree
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if(root == null ) return 0;
+    let left = maxDepth(root.left)
+    let right = maxDepth(root.right)
+    return Math.max(left,right) + 1;
 
-console.log(' Day 36 of LC 75 challenge BT DFS ')
+};
+
+
