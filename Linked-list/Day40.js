e8e1cec5baa907ac8f0a7712d218fe18c53af093
jk-nishad-72 <jkn18869@gmail.com>
2026-06-04 00:01:18 +0530

    solved

--- Linked-list/Day40.js
+++ Linked-list/Day40.js
@@ -1,3 +1,41 @@
+/**2130. Maximum Twin Sum of a Linked List
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var pairSum = function(head) {
+    let slow = head;
+    let fast = head;
+    // Step 1: Find middle
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    // Step 2: Reverse second half
+    let prev = null;
+    let curr = slow;
 
+    while (curr !== null) {
+        let nextTemp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
+    // Step 3: Find max twin sum
+    let maxSum = 0;
+    let first = head;
+    let second = prev;
+    while (second !== null) {
+        maxSum = Math.max(maxSum, first.val + second.val);
+        first = first.next;
+        second = second.next;
+    }
+    return maxSum;
+};
 
-console.log(' Day 40th of LC75 challenge linked list ')
