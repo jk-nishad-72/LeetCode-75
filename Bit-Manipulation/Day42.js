95a22d6858e96eee9cbd171966b37e2e707f0534
jk-nishad-72 <jkn18869@gmail.com>
2026-06-05 23:25:23 +0530

    solved

--- Bit-Manipulation/Day42.js
+++ Bit-Manipulation/Day42.js
@@ -1,1 +1,27 @@
-console.log(' 1318. Minimum Flips to Make a OR b Equal to c DSA DAY 42 OF LC 75 CHALLENGE ')
+/**1318. Minimum Flips to Make a OR b Equal to c
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+var minFlips = function(a, b, c) {
+    
+    let flips = 0;
+
+    for (let i = 0; i < 32; i++) {
+        let bitA = (a >> i) & 1;
+        let bitB = (b >> i) & 1;
+        let bitC = (c >> i) & 1;
+
+        if (bitC === 1) {
+            if (bitA === 0 && bitB === 0) {
+                flips += 1;
+            }
+        } else {
+            flips += bitA + bitB;
+        }
+    }
+    return flips; 
+};
+
+console.log(minFlips(a = 2, b = 6, c = 5)) 
