7a430d1f2a29ab93b3356054e8957ed61da062d1
jk-nishad-72 <jkn18869@gmail.com>
2026-06-06 23:02:35 +0530

    solved

--- BinaryTree-DFS/Day43.js
+++ BinaryTree-DFS/Day43.js
@@ -1,3 +1,25 @@
-console.log(' Day 43 of LC 75 Challenge  BT BFS  ');
+/**236. Lowest Common Ancestor of a Binary Tree
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if(root === null ) return null
+    if(root.val === p.val || root.val === q.val ) return root;
 
+        let left = lowestCommonAncestor(root.left , p, q)
+        let right = lowestCommonAncestor(root.right , p ,q)
+
+        if(left != null && right != null) return root;
+        else if(left == null || right == null) return left != null ? left : right;
+        else return null;
+};
 
