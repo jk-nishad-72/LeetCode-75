9767608c8ba2420172fb52ce1e1a1ded35c8eec3
jk-nishad-72 <jkn18869@gmail.com>
2026-06-14 23:22:05 +0530

    solved

--- Trie/app.js
+++ Trie/app.js
@@ -1,3 +1,61 @@
+//*208. Implement Trie (Prefix Tree)
+
+class TrieNode {
+    constructor() {
+        this.children = {};  // stores child nodes
+        this.isEnd = false;  // marks end of word
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    insert(word) {
+        let node = this.root;
+
+        for (let char of word) {
+            if (!node.children[char]) {
+                node.children[char] = new TrieNode();
+            }
+            node = node.children[char];
+        }
+
+        node.isEnd = true;
+    }
+
+    search(word) {
+        let node = this.root;
+
+        for (let char of word) {
+            if (!node.children[char]) return false;
+            node = node.children[char];
+        }
+
+        return node.isEnd;
+    }
+
+    startsWith(prefix) {
+        let node = this.root;
+
+        for (let char of prefix) {
+            if (!node.children[char]) return false;
+            node = node.children[char];
+        }
+
+        return true;
+    }
+}
 
 
-console.log(' Day 51 of LC75 challenge  ');
+let trie = new Trie();
+
+trie.insert("apple");
+
+console.log(trie.search("apple"));   // true
+console.log(trie.search("app"));     // false
+console.log(trie.startsWith("app")); // true
+
+trie.insert("app");
+console.log(trie.search("app"));     // true
