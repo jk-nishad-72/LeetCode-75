60a67c8f1949bd60e196ca1b35e574460632b202
jk-nishad-72 <jkn18869@gmail.com>
2026-06-15 17:06:00 +0530

    solved

--- Trie/Day52.js
+++ Trie/Day52.js
@@ -1,3 +1,26 @@
+/*
+ *1268. Search Suggestions System
+ * @param {string[]} products
+ * @param {string} searchWord
+ * @return {string[][]}
+ */
+var suggestedProducts = function (products, searchWord) {
 
+    products.sort();
+    let ans = [];
+    let ansStr = ''
+    for (let i = 0; i < searchWord.length; i++) {
+        let miniAns = []
+        ansStr += searchWord[i]
+        for (let j = 0; j < products.length; j++) {
+            if (products[j].startsWith(ansStr)) {
+                miniAns.push(products[j])
+                if (miniAns.length === 3) break;
+            }
+        }
+        ans.push(miniAns)
+    }
+    return ans
+};
+console.log(suggestedProducts(products = ["mobile", "mouse", "moneypot", "monitor", "mousepad"], searchWord = "mouse"));
 
-console.log(' Day 52 of LC 75 challenge TRIE  ')
