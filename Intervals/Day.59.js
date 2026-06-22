8d96c251154aebe42e83908926bc532185294526
jk-nishad-72 <jkn18869@gmail.com>
2026-06-22 23:58:00 +0530

    solved

--- Intervals/Day.59.js
+++ Intervals/Day.59.js
@@ -1,2 +1,26 @@
 
 console.log(' Day 59 of LC75 Challenge Intervals ');
+/**452. Minimum Number of Arrows to Burst Balloons
+ * @param {number[][]} points
+ * @return {number}
+ */
+var findMinArrowShots = function(points) {
+    if (points.length === 0) return 0;
+
+    // Step 1: Sort by end
+    points.sort((a, b) => a[1] - b[1]);
+
+    let arrows = 1;
+    let arrowPos = points[0][1];
+
+    for (let i = 1; i < points.length; i++) {
+        if (points[i][0] > arrowPos) {
+            arrows++;
+            arrowPos = points[i][1];
+        }
+    }
+
+    return arrows;
+};
+
+console.log(findMinArrowShots(points = [[10,16],[2,8],[1,6],[7,12]]));
