ae3fb9dcefcc902728e2c84bddb0ea0a90aac712
jk-nishad-72 <jkn18869@gmail.com>
2026-06-25 23:50:41 +0530

    solved

--- Graph/Day62.js
+++ Graph/Day62.js
@@ -2,3 +2,29 @@
 
 
 console.log(' Day 62 of LC75 challenge Graph');
+
+
+/**841. Keys and Rooms
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+var canVisitAllRooms = function(rooms) {
+     const visited = new Set();
+
+    function dfs(room) {
+        if (visited.has(room)) return;
+        
+        visited.add(room);
+        
+        for (let key of rooms[room]) {
+            dfs(key);
+        }
+    }
+
+    dfs(0);
+
+    return visited.size === rooms.length;
+};
+
+console.log(canVisitAllRooms(rooms = [[1],[2],[3],[]]));
+console.log(canVisitAllRooms(rooms = [[1,3],[3,0,1],[2],[0]]));
