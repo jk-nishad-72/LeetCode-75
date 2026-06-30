644ffee99455f4ae4ce65c1b078bc3daedf7503d
jk-nishad-72 <jkn18869@gmail.com>
2026-06-30 23:49:55 +0530

    solved

--- Graphs-BFS/Day67.js
+++ Graphs-BFS/Day67.js
@@ -2,3 +2,63 @@
 
 
  console.log(' Day 67 of LC75 challenge Graphs-BFS ')
+
+
+ /**994. Rotting Oranges
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var orangesRotting = function(grid) {
+    let rows = grid.length;
+    let cols = grid[0].length;
+
+    let queue = [];
+    let fresh = 0;
+
+    // Step 1: Initialize
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            if (grid[r][c] === 2) {
+                queue.push([r, c]);
+            } else if (grid[r][c] === 1) {
+                fresh++;
+            }
+        }
+    }
+
+    if (fresh === 0) return 0;
+
+    let minutes = 0;
+    let directions = [[1,0], [-1,0], [0,1], [0,-1]];
+
+    // Step 2: BFS
+    while (queue.length > 0 && fresh > 0) {
+        let size = queue.length;
+        minutes++;
+
+        for (let i = 0; i < size; i++) {
+            let [r, c] = queue.shift();
+
+            for (let [dr, dc] of directions) {
+                let nr = r + dr;
+                let nc = c + dc;
+
+                if (
+                    nr >= 0 && nr < rows &&
+                    nc >= 0 && nc < cols &&
+                    grid[nr][nc] === 1
+                ) {
+                    grid[nr][nc] = 2;
+                    fresh--;
+                    queue.push([nr, nc]);
+                }
+            }
+        }
+    }
+
+    return fresh === 0 ? minutes : -1;
+};
+
+console.log(orangesRotting(grid = [[2,1,1],[1,1,0],[0,1,1]]));
+console.log(orangesRotting(grid = [[2,1,1],[0,1,1],[1,0,1]]));
+console.log(orangesRotting( grid = [[0,2]]));
