aabd17aa5e1fbbbf201e064b8ea409d4ac45e7cb
jk-nishad-72 <jkn18869@gmail.com>
2026-07-02 00:02:11 +0530

    solved

--- DP/Day68.js
+++ DP/Day68.js
@@ -1,3 +1,23 @@
 
 
 console.log(' Day 68 of LC challenge Dyanamic programming  ');
+
+/**1137. N-th Tribonacci Number
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function(n, memo = {}) {
+    if (n === 0) return 0;
+    if (n === 1 || n === 2) return 1;
+
+    if (memo[n]) return memo[n];
+
+    memo[n] = tribonacci(n - 1, memo) +
+              tribonacci(n - 2, memo) +
+              tribonacci(n - 3, memo);
+
+    return memo[n];
+};
+
+console.log(tribonacci(4));
+console.log(tribonacci(25));
